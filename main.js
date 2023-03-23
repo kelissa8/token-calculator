0212f0c6a6e2cbc9f395f7985132715946bbd579
kelsey <128269518+kelissa8@users.noreply.github.com>
2023-03-23 22:53:27 +0000

    Main.js

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+function tokenPriceCalculator(pricePer1k, dailyBudget = null, monthlyBudget = null) {
+  let tokensPerDay, tokensPerMonth;
+  if (dailyBudget !== null) {
+    tokensPerDay = dailyBudget / pricePer1k * 1000;
+    tokensPerMonth = tokensPerDay * 30;
+  } else {
+    tokensPerDay = (monthlyBudget / 30) / pricePer1k * 1000;
+    tokensPerMonth = monthlyBudget / pricePer1k * 1000;
+  }
+
+  return [tokensPerDay, tokensPerMonth];
+}
+
+function calculateMessages(tokens, tokensPerMessage = null, messagesPerDay = null) {
+  let messages;
+  if (tokensPerMessage !== null) {
+    messages = tokens / tokensPerMessage;
+  } else {
+    tokensPerMessage = tokens / messagesPerDay;
+    messages = messagesPerDay;
+  }
+
+  return [messages, tokensPerMessage];
+}
+
+const form = document.getElementById('token-form');
+const results = document.getElementById('results');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
   const pricePer1k = parseFloat(document.getElementById('price-per-1k').value);
   const budgetType = document.getElementById('budget-type').value;
   const budget = parseFloat(document.getElementById('budget').value);
@@ -35,33 +66,3 @@
 
   results.textContent = resultText;
 });
-
-function tokenPriceCalculator(pricePer1k, dailyBudget = null, monthlyBudget = null) {
-  let tokensPerDay, tokensPerMonth;
-  if (dailyBudget !== null) {
-    tokensPerDay = dailyBudget / pricePer1k * 1000;
-    tokensPerMonth = tokensPerDay * 30;
-  } else {
-    tokensPerDay = (monthlyBudget / 30) / pricePer1k * 1000;
-    tokensPerMonth = monthlyBudget / pricePer1k * 1000;
-  }
-
-  return [tokensPerDay, tokensPerMonth];
-}
-
-function calculateMessages(tokens, tokensPerMessage = null, messagesPerDay = null) {
-  let messages;
-  if (tokensPerMessage !== null) {
-    messages = tokens / tokensPerMessage;
-  } else {
-    tokensPerMessage = tokens / messagesPerDay;
-    messages = messagesPerDay;
-  }
-
-  return [messages, tokensPerMessage];
-}
-const form = document.getElementById('token-form');
-const results = document.getElementById('results');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();

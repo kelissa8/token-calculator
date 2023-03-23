a9547dd081f2aa4061f3e57906b450152fce3576
kelsey <128269518+kelissa8@users.noreply.github.com>
2023-03-23 23:25:15 +0000

    Bugs

--- main.js
+++ main.js
@@ -17,17 +17,41 @@ function calculateMessages(tokens, tokensPerMessage = null, messagesPerDay = nul
     messages = tokens / tokensPerMessage;
   } else {
     tokensPerMessage = tokens / messagesPerDay;
-    messages = messagesPerDay;
+        messages = messagesPerDay;
   }
 
   return [messages, tokensPerMessage];
 }
 
 const form = document.getElementById('token-form');
+const tokensOnlyButton = document.getElementById('tokens-only');
 const results = document.getElementById('results');
+const additionalQuestions = document.getElementById('additional-questions');
+
+tokensOnlyButton.addEventListener('click', () => {
+  const pricePer1k = parseFloat(document.getElementById('price-per-1k').value);
+  const budgetType = document.getElementById('budget-type').value;
+  const budget = parseFloat(document.getElementById('budget').value);
+
+  let dailyBudget, monthlyBudget;
+  if (budgetType === 'daily') {
+    dailyBudget = budget;
+    monthlyBudget = null;
+  } else {
+    dailyBudget = null;
+    monthlyBudget = budget;
+  }
+
+  const [tokensPerDay, tokensPerMonth] = tokenPriceCalculator(pricePer1k, dailyBudget, monthlyBudget);
+  const resultTokens = budgetType === 'daily' ? tokensPerDay : tokensPerMonth;
+
+  results.textContent = `Tokens: ${resultTokens}`;
+});
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+
+  additionalQuestions.style.display = 'block';
 
   const pricePer1k = parseFloat(document.getElementById('price-per-1k').value);
   const budgetType = document.getElementById('budget-type').value;
